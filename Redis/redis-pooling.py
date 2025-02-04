8304c93690c403c0578464c4bc09a739964418ad
baleh <youcef.baleh@etu.u-paris.fr>
2025-02-04 14:47:40 +0100

    ajout de print pour le multithreading

--- Redis/redis-pooling.py
+++ Redis/redis-pooling.py
@@ -8,16 +8,21 @@ pool = ConnectionPool(
     port=6379,
     db=0,
     max_connections=10,
-    timeout=5,
     socket_connect_timeout=3,
     socket_keepalive=True
 )
 r = redis.Redis(connection_pool=pool)
-def worker():
+def worker(n):
     r = redis.Redis(connection_pool=pool)
     # Perform Redis operations with 'r'
+    r.set('user:{n}:name', 'John Doe'+str(n))
+    r.set('user:{n}:email', '{n}joe@h3hitema.zouloulou')
 
-threads = [threading.Thread(target=worker) for _ in range(5)]
+    user_name = r.get('user:{n}:name')
+    user_email = r.get('user:{n}:email')
+    print(user_name, user_email)
+
+threads = [threading.Thread(target=worker(i)) for i in range(5)]
 
 for thread in threads:
     thread.start()
