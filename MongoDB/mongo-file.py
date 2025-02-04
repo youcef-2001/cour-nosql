5fb4c8aeec4d6505ea3659d4b454975f5bb79e76
baleh <youcef.baleh@etu.u-paris.fr>
2025-02-04 16:56:35 +0100

    crud mango db

--- MongoDB/mongo-file.py
+++ MongoDB/mongo-file.py
@@ -8,3 +8,22 @@ collection = db.mycollection
 document = {"name": "John Doe", "email": "john.doe@example.com", "age": 30}
 result = collection.insert_one(document)
 print("Inserted document ID:", result.inserted_id)
+
+documents = [
+    {"name": "Alice", "email": "alice@example.com", "age": 25},
+    {"name": "Bob", "email": "bob@example.com", "age": 35}
+]
+result = collection.insert_many(documents)
+print("Inserted document IDs:", result.inserted_ids)
+
+
+query = {"name": "John Doe"}
+document = collection.find_one(query)
+print(document)
+
+
+query = {"age": {"$gt": 25}}
+documents = collection.find(query)
+
+for doc in documents:
+    print(doc)
